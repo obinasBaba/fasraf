c04c1ee4294dc325bb9569af54c43f97b8868366
henzzo <henokgetachew500@gmail.com>
2023-11-21 14:40:58 +0300

    fix: revalidation fix with edge

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -9,9 +9,9 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== 'fasika') {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+  // if (req.query.secret !== 'fasika') {
+  //   return res.status(401).json({ message: 'Invalid token' });
+  // }
 
   try {
     console.log('revalidating ---------> ');

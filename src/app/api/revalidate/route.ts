d2e5757598707dcc2a4c356a744350d1b02775c9
henzzo <henokgetachew500@gmail.com>
2024-09-14 13:47:28 -0400

    fix: fix the webhook revalidate hook http method -> POST

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get('secret');
 
